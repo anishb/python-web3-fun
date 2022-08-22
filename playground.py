c890e3edb8aa2192d69f26a5e202e253aee1eafe
Anish Basu <anishxbasu@gmail.com>
2022-08-22 18:26:07 -0400

    Read blocks and transactions

--- playground.py
+++ playground.py
@@ -1,4 +1,6 @@
 import os
+from datetime import datetime
+from typing import Dict, Any
 from web3 import Web3
 from web3.middleware import geth_poa_middleware
 from eth_account import Account
@@ -122,6 +124,40 @@ class Web3Playground:
 
     return self.web3.toHex(tx_hash)
 
+  def read_blocks(self) -> None:
+    latest_block: Dict[str, Any] = self.web3.eth.get_block('latest')
+    block_number: int = latest_block['number']
+    block_hash: str = latest_block['hash'].hex()
+    parent_hash: str = latest_block['parentHash'].hex()
+    num_transactions: int = len(latest_block['transactions'])
+    block_time: datetime = datetime.fromtimestamp(latest_block['timestamp'])
+
+    print("Latest block number = %d" % block_number)
+    print("Block hash = %s" % block_hash)
+    print("Parent block hash = %s" % parent_hash)
+    print("Number of transactions = %d" % num_transactions)
+    print("Block time = %s" % block_time.strftime("%m/%d/%Y, %H:%M:%S"))
+
+    for i in range(num_transactions):
+      self.read_transaction(block_number, i)
+
+
+  def read_transaction(self, block_number: int, tx_index: int) -> None:
+    tx_obj: Dict[str, Any] = self.web3.eth.get_transaction_by_block(block_number, tx_index)
+    tx_hash: str = tx_obj['hash'].hex()
+    tx_index: int = tx_obj['transactionIndex']
+    tx_from: str = tx_obj['from']
+    tx_to: str = tx_obj['to'] if tx_obj['to'] else None
+    tx_value: int = tx_obj['value']
+    tx_input: str = tx_obj['input'] if tx_obj['input'] else None
+
+    print("==========================================================")
+    print("Transaction Hash = %s" % tx_hash)
+    print("Transaction Index = %d" % tx_index)
+    print("From = %s" % tx_from)
+    print("To = %s" % tx_to)
+    print("Value = %.4f gwei" % self.web3.fromWei(tx_value, 'gwei'))
+
 
 if __name__ == "__main__":
   load_dotenv()
@@ -143,6 +179,9 @@ if __name__ == "__main__":
 
   # Interact with greeter contract
   print("Current greeting = %s" % playground.get_greeting())
-  tx_hash = playground.set_greeting('Hola!')
+  #tx_hash = playground.set_greeting('Hola!')
   #print("Set new greeting. TX hash = %s" % tx_hash)
   #print("New greeting = %s" % playground.get_greeting())
+
+  # Read block data
+  playground.read_blocks()
